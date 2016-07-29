732df5a642cc0637b6c72fac5ad8c8933316a058
Abey Onalaja <abey.onalaja@gmail.com>
2016-07-29 11:25:24 +0100

    Complete adding player resources

--- src/Update.elm
+++ src/Update.elm
@@ -11,6 +11,6 @@ update msg model =
     PlayersMsg subMsg ->
       let
           (updatePlayers, cmd) =
-            Players.UPdate.update subMsg model.players
+            Players.Update.update subMsg model.players
       in
           ( { model | players = updatePlayers }, Cmd.map PlayersMsg cmd )
