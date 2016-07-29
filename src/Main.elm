188ac1474716dca616fdd2f0437ba865f72aab83
Abey Onalaja <abey.onalaja@gmail.com>
2016-07-29 20:44:26 +0100

    Add commands module

--- src/Main.elm
+++ src/Main.elm
@@ -2,16 +2,17 @@ module Main exposing(..)
 
 
 import Html.App
-import Messages exposing (Msg)
+import Messages exposing (Msg(..))
 import Models exposing (Model, initialModel)
 import View exposing (view)
 import Update exposing (update)
+import Players.Commands exposing (fectchAll)
 
 
 
 init : (Model, Cmd Msg)
 init =
-  ( initialModel, Cmd.none)
+  ( initialModel, Cmd.map PlayersMsg fectchAll)
 
 
 
--- src/Models.elm
+++ src/Models.elm
@@ -8,4 +8,4 @@ type alias Model =
 
 initialModel : Model
 initialModel =
-  { players = [ Player 1 "Sam" 1 ] }
+  { players = [ ] }
