df1602d2eab73e71a1e43206ea30d442b30e9323
Abey Onalaja <abey.onalaja@gmail.com>
2016-07-29 23:48:03 +0100

    Added navigation

--- src/Models.elm
+++ src/Models.elm
@@ -3,14 +3,15 @@ module Models exposing (..)
 import Players.Models exposing (Player)
 import Routing
 
+
 type alias Model =
-  { players : List Player
-  , route : Routing.Route
-  }
+    { players : List Player
+    , route : Routing.Route
+    }
 
 
 initialModel : Routing.Route -> Model
 initialModel route =
-  { players = [ ]
-  , route = route
-  }
+    { players = []
+    , route = route
+    }
--- src/Players/Commands.elm
+++ src/Players/Commands.elm
@@ -1,31 +1,31 @@
 module Players.Commands exposing (..)
 
-
 import Http
 import Json.Decode as Decode exposing ((:=))
 import Task
 import Players.Models exposing (PlayerId, Player)
 import Players.Messages exposing (..)
 
-fectchAll : Cmd Msg
-fectchAll =
-  Http.get collectionDecoder fectchAllUrl
-    |> Task.perform FetchAllFail FetchAllDone
+
+fetchAll : Cmd Msg
+fetchAll =
+    Http.get collectionDecoder fetchAllUrl
+        |> Task.perform FetchAllFail FetchAllDone
 
 
-fectchAllUrl : String
-fectchAllUrl =
-  "http://localhost:4000/players"
+fetchAllUrl : String
+fetchAllUrl =
+    "http://localhost:4000/players"
 
 
 collectionDecoder : Decode.Decoder (List Player)
 collectionDecoder =
-  Decode.list memberDecoder
+    Decode.list memberDecoder
 
 
 memberDecoder : Decode.Decoder Player
 memberDecoder =
-  Decode.object3 Player
-    ( "id" := Decode.int )
-    ( "name" := Decode.string )
-    ( "level" := Decode.int )
+    Decode.object3 Player
+        ("id" := Decode.int)
+        ("name" := Decode.string)
+        ("level" := Decode.int)
